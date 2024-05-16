54d4bc9324c7c654af51407f454de6fb59577d48
79204 <rozhkov-aleksander.rozhkov@yandex.ru>
2024-05-16 23:52:19 +0300

    initial commit

---
+++ src/main/java/com/rozhkov/callcenter/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.rozhkov.callcenter.controller;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@NoArgsConstructor
+public class UserController {
+}
---
+++ src/main/java/com/rozhkov/callcenter/service/UserService.java
@@ -0,0 +1,7 @@
+package com.rozhkov.callcenter.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
