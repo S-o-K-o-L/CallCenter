49ab31c10630b3d2864b58c1f6b73dd7ee4201cb
rozhkov <rozhkov-aleksander.rozhkov@yandex.ru>
2024-05-21 19:18:07 +0300

    add jwt auth

--- src/main/java/com/rozhkov/callcenter/CallCenterApplication.java
+++ src/main/java/com/rozhkov/callcenter/CallCenterApplication.java
@@ -2,14 +2,8 @@ package com.rozhkov.callcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.rozhkov.callcenter.repository")
-@ComponentScan("com.rozhkov.callcenter.entity")
 public class CallCenterApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/rozhkov/callcenter/configuration/SecurityConfiguration.java
+++ src/main/java/com/rozhkov/callcenter/configuration/SecurityConfiguration.java
@@ -1,45 +1,51 @@
 package com.rozhkov.callcenter.configuration;
 
 import com.rozhkov.callcenter.service.UserService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-@RequiredArgsConstructor
-@EnableMethodSecurity(securedEnabled = true)
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Slf4j
-public class SecurityConfiguration  {
-    private final UserService userService;
-    private final JwtRequestFilter jwtRequestFilter;
+public class SecurityConfiguration {
+    private  UserService userService;
+    private  JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setJwtRequestFilter(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        log.info("enter chain");
+        System.out.println("enter 2");
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/index.html", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/index", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/secured").authenticated()
-
                         .requestMatchers("/info").authenticated()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().permitAll())
@@ -49,7 +55,7 @@ public class SecurityConfiguration  {
                         -> exceptionHandling.authenticationEntryPoint(
                                 new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        log.info("exit chain");
+
         return http.build();
     }
 
@@ -59,6 +65,11 @@ public class SecurityConfiguration  {
         daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
         daoAuthenticationProvider.setUserDetailsService(userService);
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
